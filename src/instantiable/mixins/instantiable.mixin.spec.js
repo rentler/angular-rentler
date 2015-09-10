0c750b79b778262a1c232d9fb3df806451a9f13e
Noctis <chrishtanaka@users.noreply.github.com>
2015-09-10 14:45:51 -0600

    Adds iife to datamodel and instantiable mixin specs

--- src/instantiable/mixins/instantiable.mixin.spec.js
+++ src/instantiable/mixins/instantiable.mixin.spec.js
@@ -1,46 +1,48 @@
-'use strict';
-
-describe('Model', function () {
-  var User;
+(function () {
+  'use strict';
   
-  beforeEach(module('rentler.core', function ($provide) {
-	
-	$provide.factory('User', UserFactory);
-	
-	$provide.factory.$inject = ['Instantiable'];
-	
-	function UserFactory(Instantiable) {
-	  var model = {
-      fullName: function () {
-          var _this = this;
-          return _this.firstName + ' ' + _this.lastName;
-        }
-      };
-      
-      _.assign(model, _.cloneDeep(Instantiable));
-      
-      return model;
+  describe('Model', function () {
+    var User;
+    
+    beforeEach(module('rentler.core', function ($provide) {
+    
+    $provide.factory('User', UserFactory);
+    
+    $provide.factory.$inject = ['Instantiable'];
+    
+    function UserFactory(Instantiable) {
+      var model = {
+        fullName: function () {
+            var _this = this;
+            return _this.firstName + ' ' + _this.lastName;
+          }
+        };
+        
+        _.assign(model, _.cloneDeep(Instantiable));
+        
+        return model;
+      }
+    }));
+    
+    beforeEach(inject(function (_User_) {
+    User = _User_;
+    }));
+    
+    it('should be able to create instances', function () {
+    var user = User.create({ firstName: 'John', lastName: 'Doe' });
+  
+      expect(user.firstName).toBe('John');
+      expect(user.lastName).toBe('Doe');
+    });
+    
+    it('should bind function correctly for callbacks', function () {
+    var user = User.create({ firstName: 'John', lastName: 'Doe' });
+    
+    function test(cb) {
+      return cb();
     }
-  }));
-  
-  beforeEach(inject(function (_User_) {
-	User = _User_;
-  }));
-  
-  it('should be able to create instances', function () {
-	var user = User.create({ firstName: 'John', lastName: 'Doe' });
-
-    expect(user.firstName).toBe('John');
-    expect(user.lastName).toBe('Doe');
+    
+    expect(test(user.fullName)).toBe('John Doe');
+    });
   });
-  
-  it('should bind function correctly for callbacks', function () {
-	var user = User.create({ firstName: 'John', lastName: 'Doe' });
-	
-	function test(cb) {
-	  return cb();
-	}
-	
-	expect(test(user.fullName)).toBe('John Doe');
-  });
-});
+}());
